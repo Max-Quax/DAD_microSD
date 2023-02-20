ccd39328a5c9b9efb4f5dc623b914a0250e31005
Max <Max@10.0.0.248>
2023-02-20 16:37:57 -0500

    Wrote a test to check how long it takes to write 2048 chars

--- DAD_Write_To_microSD/DAD_microSD_Test.c
+++ DAD_Write_To_microSD/DAD_microSD_Test.c
@@ -9,6 +9,7 @@
 // HAL includes
 #include <HAL/DAD_microSD.h>
 #include <HAL/DAD_UART.h>
+#include <HAL/DAD_Timer.h>
 
 int main(void)
 {
@@ -24,6 +25,24 @@ int main(void)
     char *message[] = {"one", "two", "three", "four"};
     DAD_microSD_write(fileName, message, 4, &microSD_UART);
 
+    //
+    //Timed Test
+    //
+    // Timer setup
+    double timeElapsed;
+    Timer_A_UpModeConfig timerCfg;
+    DAD_Timer_Initialize_ms(60000, TIMER_A1_BASE, &timerCfg);
+
+    // Generate test string
+    int i;
+    char msg[2049] = "t";
+    for(i = 0; i < 2048; i++)
+    strcat(msg, "t");
+
+    // Send test string over UART
+    DAD_Timer_Start(TIMER_A1_BASE);                         // Begin test timer
+    DAD_UART_Write_Str(&microSD_UART, msg);
+    timeElapsed = DAD_Timer_Stop(TIMER_A1_BASE, &timerCfg); // End test timer
 
     while(1); // Trap CPU
 }
